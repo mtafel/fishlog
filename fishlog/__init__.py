0fbaa06d76b5352862a8c9f1f0b35ba2eca50f10
Mike <miketafel@hostgis.com>
2012-01-18 22:05:02 -0700

    Setup the first views and models in the application

--- fishlog/__init__.py
+++ fishlog/__init__.py
@@ -1,16 +1,27 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-
-from .models import DBSession
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+#from tutorial.security import groupfinder
+from .models import DBSession, initialize_sql
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    #engine = engine_from_config(settings, 'sqlalchemy.')
+    #DBSession.configure(bind=engine)
+    #authn_policy = AuthTktAuthenticationPolicy('sosecret', callback=groupfinder)
+    #authz_policy = ACLAuthorizationPolicy()
     engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
+    initialize_sql(engine)
 
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'fishlog:static')
+    config.add_route('view_fishlog', '/')
+    config.add_route('view_page', '/{pagename}')
+    #config.add_route('add_page', '/add_page/{pagename}')
+    #config.add_route('edit_page', '/{pagename}/edit_page')
+    config.add_view('fishlog.views.view_fishlog', route_name='view_fishlog')
+    config.add_view('fishlog.views.view_page', route_name='view_page',
+                    renderer='fishlog:templates/view.pt')
+    return config.make_wsgi_app()
--- fishlog/views.py
+++ fishlog/views.py
@@ -1,11 +1,25 @@
+import re
+#from docutils.core import publish_parts
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
+from pyramid.url import route_url
 
 from .models import (
     DBSession,
-    MyModel,
+    Page,
     )
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
-    return {'one':one, 'project':'FishLog'}
+#@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def view_fishlog(request):
+    return HTTPFound(location = route_url('view_page',request,           pagename='FrontPage'))
+def view_page(request):
+    pagename = request.matchdict['pagename']
+    session = DBSession()
+    page = session.query(Page).filter_by(name=pagename).first()
+    if page is None:
+        return HTTPNotFound('No such page')
+
+    #content = publish_parts(page.data, writer_name='html')['html_body']
+    content = "Hello World"
+    return dict(page=page, content=content)
+
